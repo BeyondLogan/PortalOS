312d4afaaeae400600bc1c766a07a72807cb6faf
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2023-11-16 17:22:18 +1030

    Update InstallPack.sh

--- InstallPack.sh
+++ InstallPack.sh
@@ -1,3 +1,14 @@
+#Remove Sources list and replace it with a custom list
+cd /etc/apt/
+rm sources.list
+wget https://raw.githubusercontent.com/BeyondLogan/Sources/main/sources.list
+cd
+clear
+
+#update and upgrade os
+apt update
+apt upgrade -y
+
 # Install packages
 apt install gnome-session gnome-terminal nano curl wget firefox-esr qemu-system qemu-user-static libvirt-clients bridge-utils virt-manager ovmf python3 python3-full python3-tk pip tint2 gnome-tweaks dolphin nautilus -y
 reboot
