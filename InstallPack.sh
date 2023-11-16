46c96387264393a410e3c48ec0e06aad13646d99
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2023-11-16 17:48:48 +1030

    Update InstallPack.sh

--- InstallPack.sh
+++ InstallPack.sh
@@ -11,4 +11,11 @@ apt upgrade -y
 
 # Install packages
 apt install gnome-session gnome-terminal nano curl wget firefox-esr qemu-system qemu-user-static libvirt-clients bridge-utils virt-manager ovmf python3 python3-full python3-tk pip tint2 gnome-tweaks dolphin nautilus -y
+clear
+
+#Download Network Config Fix
+cd
+cd /etc/NetworkManager/
+rm NetworkManager.conf
+wget https://raw.githubusercontent.com/BeyondLogan/PortalOS/main/NetworkManager.conf
 reboot
