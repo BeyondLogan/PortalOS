055e78578686977b839414c1754dad693dc181f2
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2023-11-16 18:13:39 +1030

    Updated config

--- installpack.sh
+++ installpack.sh
@@ -1,4 +1,4 @@
-#Remove Sources list and replace it with a custom list
+#Remove Sources System list and replace it with a custom list
 cd /etc/apt/
 rm sources.list
 wget https://raw.githubusercontent.com/BeyondLogan/Sources/main/sources.list
@@ -8,14 +8,18 @@ cd
 apt update
 apt upgrade -y
 
-# Install packages
+# Install Packages
 apt install gnome-session gnome-terminal nano curl wget firefox-esr qemu-system qemu-user-static libvirt-clients bridge-utils virt-manager ovmf python3 python3-full python3-tk pip tint2 gnome-tweaks dolphin nautilus -y
 
-#Download Network Config Fix
+# Download Network Config Fix
 cd
 cd /etc/NetworkManager/
 rm NetworkManager.conf
-wget https://raw.githubusercontent.com/BeyondLogan/PortalOS/main/NetworkManager.conf
+wget https://raw.githubusercontent.com/BeyondLogan/PortalOS/main/SYSConfig/NetworkManager.conf
 
 #Done
+echo
+echo
 echo If everything downloaded fine with no issue, run the reboot command
+echo
+echo
