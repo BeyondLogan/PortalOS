52a0d00bf43266da23641027d191e91cc9efa9a0
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2023-11-16 17:49:59 +1030

    Update InstallPack.sh

--- InstallPack.sh
+++ InstallPack.sh
@@ -3,7 +3,6 @@ cd /etc/apt/
 rm sources.list
 wget https://raw.githubusercontent.com/BeyondLogan/Sources/main/sources.list
 cd
-clear
 
 #update and upgrade os
 apt update
@@ -11,11 +10,12 @@ apt upgrade -y
 
 # Install packages
 apt install gnome-session gnome-terminal nano curl wget firefox-esr qemu-system qemu-user-static libvirt-clients bridge-utils virt-manager ovmf python3 python3-full python3-tk pip tint2 gnome-tweaks dolphin nautilus -y
-clear
 
 #Download Network Config Fix
 cd
 cd /etc/NetworkManager/
 rm NetworkManager.conf
 wget https://raw.githubusercontent.com/BeyondLogan/PortalOS/main/NetworkManager.conf
-reboot
+
+#Done
+echo If everything downloaded fine with no issue, run the reboot command
