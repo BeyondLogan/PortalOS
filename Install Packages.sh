7b406c7e00fe1f081d5381ec1816ed6b54a64703
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2023-11-16 17:14:32 +1030

    Parts/Install/

---
+++ Install Packages.sh
@@ -0,0 +1,3 @@
+# Install packages
+apt install gnome-session gnome-terminal nano curl wget firefox-esr qemu-system qemu-user-static libvirt-clients bridge-utils virt-manager ovmf python3 python3-full python3-tk pip tint2 gnome-tweaks Dolphin Nautilus -y
+reboot
